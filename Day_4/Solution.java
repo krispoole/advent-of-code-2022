c1b53e89cbc626efb6d232b557860fae86b597e3
Kris Poole <krispoole@pm.me>
2022-12-04 11:49:43 -0600

    Day_4 added

---
+++ Day_4/Solution.java
@@ -0,0 +1,43 @@
+package Day_4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        String eachLine;
+        int dataCounter = 0;
+        int counter = 0;
+
+        
+        File file = new File("C:\\Users\\krisp\\Code\\advent-of-code-2022\\Day_4\\data.txt");
+ 
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        while ((eachLine = br.readLine()) != null) {
+
+            String[] dataArrayString = eachLine.split("[-\\,]");
+            int[] dataArrayInt = new int[4];
+
+            for (int i = 0; i < dataArrayString.length; i++) {
+                dataArrayInt[i] = Integer.parseInt(dataArrayString[i]);
+            }
+
+            if (dataArrayInt[0] <= dataArrayInt[2] && dataArrayInt[1] >= dataArrayInt[3]) {
+                counter++;
+            }
+
+            else if (dataArrayInt[2] <= dataArrayInt[0] && dataArrayInt[3] >= dataArrayInt[1]) {
+                counter++;
+            }
+
+            dataCounter++;
+        }
+
+        System.out.println("dataCounter: " + dataCounter);
+        System.out.println("counter: " + counter);
+    }
+}
